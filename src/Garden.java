8a89651a114958b6ae9803d6a2582d692856fe8e
xminarikd <32222554+xminarikd@users.noreply.github.com>
2019-03-29 23:14:49 +0100

    blok reaction

--- src/Garden.java
+++ src/Garden.java
@@ -32,28 +32,93 @@ public class Garden {
 
     public Suradnica getStartindex(int index){
         if(index < sirka){
-            return new Suradnica(0,index,0,1);
+            return new Suradnica(0,index,0,1, index);
         }
         else if(index < vyska + sirka){
-            return new Suradnica(index - sirka,sirka-1,-1,0);
+            return new Suradnica(index - sirka,sirka-1,-1,0, index);
         }
         else if(index < (2 * sirka) + vyska){
-            return new Suradnica(vyska - 1,index - (vyska + sirka),0,-1);
+            return new Suradnica(vyska - 1,index - (vyska + sirka),0,-1, index);
         }
         else{
-            return new Suradnica(index - ((2 * sirka) + vyska),0,1,0);
+            return new Suradnica(index - ((2 * sirka) + vyska),0,1,0, index);
+        }
+    }
+
+    public Suradnica changeDirection(Suradnica suradnica){
+        //hore/dole
+        if(suradnica.pohybRiadok == 0){
+            if((suradnica.stlpec + 1 < sirka)  &&  board[suradnica.riadok][suradnica.stlpec+1] == 0)
+                return new Suradnica(suradnica.riadok, suradnica.stlpec + 1, 1, 0);
+
+            else if((suradnica.stlpec - 1 < sirka && suradnica.stlpec - 1 >= 0)  &&  board[suradnica.riadok][suradnica.stlpec-1] == 0)
+                return new Suradnica(suradnica.riadok, suradnica.stlpec - 1, -1, 0);
+
+            else if(suradnica.stlpec + 1 == sirka || suradnica.stlpec - 1 < 0)
+                return new Suradnica(-1,-1,-1,-1,-1);
+
+            else return null;
+        }
+        //lavo/pravo
+        else{
+            if((suradnica.riadok + 1 < vyska)  &&  board[suradnica.riadok+1][suradnica.stlpec] == 0)
+                return new Suradnica(suradnica.riadok + 1, suradnica.stlpec, 0, 1);
+
+            else if((suradnica.riadok - 1 < vyska && suradnica.riadok - 1 >= 0)  &&  board[suradnica.riadok-1][suradnica.stlpec] == 0)
+                return new Suradnica(suradnica.riadok - 1, suradnica.stlpec, 0, -1);
+
+            else if(suradnica.riadok + 1 == vyska || suradnica.riadok - 1 < 0)
+                return new Suradnica(-1,-1,-1,-1,-1);
+
+            else return null;
+        }
+    }
+
+    public Suradnica findNewBegin(Suradnica suradnica){
+        int tmp = suradnica.bIndex;
+        while(true){
+            tmp++;
+            if(tmp > (2*vyska + 2*sirka - 1)){
+                tmp = 0;
+            }
+            if(tmp == suradnica.bIndex){
+                return null;
+            }
+            if(canWalk(getStartindex(tmp))){
+                return getStartindex(tmp);
+            }
         }
     }
 
     public void walkGarden(ArrayList chromozome){
         Suradnica suradnica;
+        Suradnica tmpsur;
+        boolean blocked = false;
         int poradie = 1;
         for(int i = 0; i < maxGene; i++){
             suradnica = getStartindex((int) chromozome.get(i));
+            suradnica.setbIndex((int) chromozome.get(i));
+            if(!canWalk(suradnica)) {
+                suradnica = findNewBegin(suradnica);
+            }
+            if(suradnica == null){
+                System.out.println("Something wrong");
+                break;
+            }
+            chromozome.set(i,suradnica.bIndex);
+                while(in(suradnica)){
 
-            if(canWalk(suradnica)){
+                    if(!canWalk(suradnica)){
+                        suradnica.decSuradnica();
+                        suradnica = changeDirection(suradnica);
+                        if(suradnica == null){
+                            blocked = true;
+                            break;
+                        }
+                        if(suradnica.bIndex == -1)
+                            break;
+                    }
 
-                while(canWalk(suradnica)){
                     board[suradnica.riadok][suradnica.stlpec] = poradie;
                     suradnica.incrementSuradnica();
                 }
@@ -61,19 +126,20 @@ public class Garden {
 
                 System.out.println(Arrays.deepToString(board).replaceAll("], ", "]" + System.lineSeparator()));
                 System.out.println();
-            }
+
         }
 
       System.out.println(Arrays.deepToString(board).replaceAll("], ", "]" + System.lineSeparator()));
     }
 
 
+    public boolean in(Suradnica suradnica){
+        return (suradnica.riadok < vyska && suradnica.riadok >= 0) && (suradnica.stlpec < sirka && suradnica.stlpec >= 0);
+    }
 
 
     private boolean canWalk(Suradnica suradnica){
-        if((suradnica.riadok < vyska && suradnica.riadok >= 0) && (suradnica.stlpec < sirka && suradnica.stlpec >= 0))
-            return board[suradnica.riadok][suradnica.stlpec] == 0;
-        else return false;
+            return (board[suradnica.riadok][suradnica.stlpec] == 0);
     }
 
 
--- src/Suradnica.java
+++ src/Suradnica.java
@@ -3,6 +3,7 @@ public class Suradnica {
     public int stlpec;
     public int pohybRiadok;
     public int pohyvStlpec;
+    public int bIndex;
 
 
     public Suradnica(int riadok, int stlpec, int pohybRiadok, int pohyvStlpec) {
@@ -12,9 +13,25 @@ public class Suradnica {
         this.pohyvStlpec = pohyvStlpec;
     }
 
+    public Suradnica(int riadok, int stlpec, int pohybRiadok, int pohyvStlpec, int bIndex) {
+        this.riadok = riadok;
+        this.stlpec = stlpec;
+        this.pohybRiadok = pohybRiadok;
+        this.pohyvStlpec = pohyvStlpec;
+        this.bIndex = bIndex;
+    }
+
     public void incrementSuradnica(){
         riadok += pohyvStlpec;
         stlpec += pohybRiadok;
     }
 
+    public void decSuradnica(){
+        riadok -= pohyvStlpec;
+        stlpec -= pohybRiadok;
+    }
+
+    public void setbIndex(int bIndex) {
+        this.bIndex = bIndex;
+    }
 }
